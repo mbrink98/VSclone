fb558f880643269619f7b5da7555301237ec0024
Kiên Nguyên <ki.nguyen@ostfalia.de>
2024-01-17 19:21:29 +0100

    fixed div 0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,86 +11,103 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
 
-    public bool gameIsPaused{
+    public bool gameIsPaused
+    {
         get;
         set;
     }
 
-    public Queue<float> levels{
+    public Queue<float> levels
+    {
         get;
         set;
     }
-    public float [] levelsArray{
+    public float[] levelsArray
+    {
         get;
         set;
     }
 
-    public GameObject player{
+    public GameObject player
+    {
         get;
         private set;
     }
 
-    public float playerEXP{
+    public float playerEXP
+    {
         get;
         set;
     }
 
-    public float playerLVL{
+    public float playerLVL
+    {
         get;
         set;
     }
 
-    public float playerHealth{
+    public float playerHealth
+    {
         get;
         set;
     }
 
-    public float playerMaxHealth{
+    public float playerMaxHealth
+    {
         get;
         set;
     }
 
-    public float playerMovementSpeed{
+    public float playerMovementSpeed
+    {
         get;
         set;
     }
 
-    public float playerRotationSpeed{
+    public float playerRotationSpeed
+    {
         get;
         set;
     }
 
-    public float playerBulletSpeed{
+    public float playerBulletSpeed
+    {
         get;
         set;
     }
 
-    public float playerAttackDelay{
+    public float playerAttackDelay
+    {
         get;
         set;
     }
 
-    public float playerReloadSpeed{
+    public float playerReloadSpeed
+    {
         get;
         set;
     }
 
-    public float playerAmmo{
+    public float playerAmmo
+    {
         get;
         set;
     }
 
-    public float playerMaxAmmo{
+    public float playerMaxAmmo
+    {
         get;
         set;
     }
 
-    public string playerGun{
+    public string playerGun
+    {
         get;
         set;
     }
 
-    public int enemyMaxHealth{
+    public int enemyMaxHealth
+    {
         get;
         set;
     }
@@ -114,7 +131,7 @@ public class GameManager : MonoBehaviour
     public Image ExpBar;
     public float expCap = 3f;
     public float expMulti = 1.4f;
-    
+
 
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private GameObject enemySpawnerPrefab;
@@ -147,18 +164,18 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         Queue<float> levels = new Queue<float>();
-        
+
         levels.Enqueue(expCap);
-        for (int i = 1; i < 50; i++) 
+        for (int i = 1; i < 50; i++)
         {
-            expCap = (float) Math.Round(expCap * expMulti);
+            expCap = (float)Math.Round(expCap * expMulti);
             levels.Enqueue(expCap);
         }
         this.levels = levels;
         this.levelsArray = levels.ToArray();        //copy to array um pro lvl auf exp nötig zum aufsteigen zuzugreifen
 
         this.playerGun = "Gun";
-     
+
         this.gameIsPaused = false;
 
         _instance = this;
@@ -172,10 +189,10 @@ public class GameManager : MonoBehaviour
         GenerateWeapons();
 
         // Enemy Spawner spawn
-        Vector3 spawnLeftBorder = new Vector3(transform.position.x - 5, transform.position.y,transform.position.z);
-        Vector3 spawnRightBorder = new Vector3(transform.position.x + 5, transform.position.y,transform.position.z);
-        Vector3 spawnBottomBorder = new Vector3(transform.position.x, transform.position.y  - 5,transform.position.z);
-        Vector3 spawnTopBorder = new Vector3(transform.position.x, transform.position.y + 5,transform.position.z);
+        Vector3 spawnLeftBorder = new Vector3(transform.position.x - 5, transform.position.y, transform.position.z);
+        Vector3 spawnRightBorder = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
+        Vector3 spawnBottomBorder = new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
+        Vector3 spawnTopBorder = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
 
         _instance.enemySpawner1 = Instantiate(enemySpawnerPrefab, spawnLeftBorder, Quaternion.identity);
         _instance.enemySpawner2 = Instantiate(enemySpawnerPrefab, spawnRightBorder, Quaternion.identity);
@@ -185,32 +202,37 @@ public class GameManager : MonoBehaviour
 
 
         gameManagerStarted.Invoke();
-    } 
+    }
 
     void Update()
     {
-        spawnerTimer += Time.deltaTime;
-        EnemyTimer += Time.deltaTime;
-        if(spawnerTimer >= period ) { // reset position to borders
-            spawnerTimer = spawnerTimer % 1f;
-            Vector3 bottomLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(-1, -1, Camera.main.nearClipPlane));
-            Vector3 topLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(-1, Camera.main.pixelHeight+1, Camera.main.nearClipPlane));
-            Vector3 topRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth+1, Camera.main.pixelHeight+1, Camera.main.nearClipPlane));
-            Vector3 bottomRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth+1, -1, Camera.main.nearClipPlane));
+        if (!_instance.gameIsPaused)
+        {
+            spawnerTimer += Time.deltaTime;
+            EnemyTimer += Time.deltaTime;
+            if (spawnerTimer >= period)
+            { // reset position to borders
+                spawnerTimer = spawnerTimer % 1f;
+                Vector3 bottomLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(-1, -1, Camera.main.nearClipPlane));
+                Vector3 topLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(-1, Camera.main.pixelHeight + 1, Camera.main.nearClipPlane));
+                Vector3 topRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth + 1, Camera.main.pixelHeight + 1, Camera.main.nearClipPlane));
+                Vector3 bottomRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth + 1, -1, Camera.main.nearClipPlane));
 
-            _instance.enemySpawner1.transform.position = bottomLeft;
-            _instance.enemySpawner2.transform.position = topLeft;
-            _instance.enemySpawner3.transform.position = topRight;
-            _instance.enemySpawner4.transform.position = bottomRight;
-        }
+                _instance.enemySpawner1.transform.position = bottomLeft;
+                _instance.enemySpawner2.transform.position = topLeft;
+                _instance.enemySpawner3.transform.position = topRight;
+                _instance.enemySpawner4.transform.position = bottomRight;
+            }
 
-        if(EnemyTimer >= EnemyHealthCycle ) {
-            EnemyTimer = spawnerTimer % 1f;
-            _instance.enemyMaxHealth += 1;
+            if (EnemyTimer >= EnemyHealthCycle)
+            {
+                EnemyTimer = spawnerTimer % 1f;
+                _instance.enemyMaxHealth += 1;
+            }
+
+            healthBar.fillAmount = Mathf.Clamp(_instance.playerHealth / _instance.playerMaxHealth, 0, 1);               //könnte vllt ins playergetshitevent
+            ExpBar.fillAmount = Mathf.Clamp(_instance.playerEXP / _instance.levels.Peek(), 0, 1);         //könnte vllt ins player gets lvl up event
         }
-        
-        healthBar.fillAmount = Mathf.Clamp(_instance.playerHealth/_instance.playerMaxHealth,0,1);               //könnte vllt ins playergetshitevent
-        ExpBar.fillAmount = Mathf.Clamp(_instance.playerEXP/_instance.levels.Peek(),0,1);         //könnte vllt ins player gets lvl up event
     }
 
     public void GenerateWeapons()
@@ -225,10 +247,10 @@ public class GameManager : MonoBehaviour
             Vector3 weaponPosition = new Vector3(canvas.transform.position.x + positionsX.Dequeue(), canvas.transform.position.y, canvas.transform.position.z);
 
             GameObject weaponChoice = _instance.upgradePrefab;
-            weaponChoice.name = "Weapon-"+ weapons[i];
+            weaponChoice.name = "Weapon-" + weapons[i];
             Sprite weaponSprite = Resources.Load<Sprite>("Sprites/" + weapons[i]);
             weaponChoice.GetComponent<Image>().sprite = weaponSprite;
-            weaponChoice.GetComponentInChildren<TMP_Text>().text = weapons[i]+ ": "+ weaponsDescription[i];
+            weaponChoice.GetComponentInChildren<TMP_Text>().text = weapons[i] + ": " + weaponsDescription[i];
 
             _instance.generatedChoices.Add(Instantiate(weaponChoice, weaponPosition, Quaternion.identity, canvas.transform));
         }
@@ -240,11 +262,11 @@ public class GameManager : MonoBehaviour
         _instance.playerEXP = 0f;
         _instance.playerLVL = 1f;
 
-        
+
         switch (upgradeName)
         {
             // Weapons
-            case "Gun": 
+            case "Gun":
                 _instance.playerBulletSpeed = 10f;
                 _instance.playerAttackDelay = 0.0f;
                 _instance.playerMaxAmmo = 10f;
