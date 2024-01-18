eb31918e158d68789a1837c3585d00516f49accf
Philipp Sander <pcsander1@gmx.de>
2024-01-18 21:56:07 +0100

    Stats and FPS hideable

--- Assets/FrameRate.cs
+++ Assets/FrameRate.cs
@@ -6,17 +6,38 @@ using TMPro;
 public class FrameRate : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
+
+    public GameObject statDisplay;
+
+    private DisplayStats statsScript;
+
+    private float timeSinceUpdate;
+
+    private int frameCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        statsScript = statDisplay.GetComponent<DisplayStats>();
+        timeSinceUpdate = 0.0f;
+        frameCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        float fps = 1f/Time.unscaledDeltaTime;
-        textMeshPro.text = Mathf.RoundToInt(fps).ToString();
+        if (statsScript.showStats){
+            frameCount++;
+            timeSinceUpdate += Time.unscaledDeltaTime;
+            if (timeSinceUpdate >= .5f) {
+                textMeshPro.SetText("{0}", frameCount*2);
+                timeSinceUpdate -= .5f;
+                frameCount = 0;
+            }
+        } else {
+            textMeshPro.text = "";
+            timeSinceUpdate = 0.0f;
+            frameCount = 0;
+        }
     }
 }
--- Assets/Scripts/UpgradeCards/DisplayStats.cs
+++ Assets/Scripts/UpgradeCards/DisplayStats.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 
 public class DisplayStats : MonoBehaviour
 {
+    public bool showStats
+    {
+        get;
+        private set;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        showStats = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<TMP_Text>().text = 
-            $"Level:{GameManager.Instance.playerLVL}\n" +
-            $"EXP:{GameManager.Instance.playerEXP}\n" +
-            $"Max HP:{GameManager.Instance.playerMaxHealth}\n" +
-            $"HP:{GameManager.Instance.playerHealth}\n" +
-            $"MovementSpeed:{GameManager.Instance.playerMovementSpeed}\n" +
-            $"Ammo:{GameManager.Instance.playerAmmo}\n" +
-            $"ReloadLength:{GameManager.Instance.playerReloadSpeed}\n" +
-            $"AttackDelay:{GameManager.Instance.playerAttackDelay}\n" +
-            $"EnemyMaxHealth:{GameManager.Instance.enemyMaxHealth}\n"
-        ;
+        if (Input.GetKeyDown(KeyCode.F3)){
+            showStats = !showStats;
+        }
+        if (showStats){
+            GetComponent<TMP_Text>().text = 
+                "Game stats (F3)\n" +
+                $"Level:{GameManager.Instance.playerLVL}\n" +
+                $"EXP:{GameManager.Instance.playerEXP}\n" +
+                $"Max HP:{GameManager.Instance.playerMaxHealth}\n" +
+                $"HP:{GameManager.Instance.playerHealth}\n" +
+                $"MovementSpeed:{GameManager.Instance.playerMovementSpeed}\n" +
+                $"Ammo:{GameManager.Instance.playerAmmo}\n" +
+                $"ReloadLength:{GameManager.Instance.playerReloadSpeed}\n" +
+                $"AttackDelay:{GameManager.Instance.playerAttackDelay}\n" +
+                $"EnemyMaxHealth:{GameManager.Instance.enemyMaxHealth}\n"
+            ;
+        } else {
+            GetComponent<TMP_Text>().text = "";
+        }
     }
 }
